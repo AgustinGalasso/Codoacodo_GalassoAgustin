5363cc1c394f6ce1f2df5b1fb5d75b4c5365a6b1
AgustinGalasso <136178437+AgustinGalasso@users.noreply.github.com>
2023-11-04 15:50:52 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+// definir el valor  del Ticket
+const ticket = 200;
+
+//definir descuentos
+
+const descEstudiante = 0.80;
+const descTrainee = 0.50;
+const descJunior = 0.15;
+
+//Tomar los elementos
+
+const cantidad = document.getElementById('cantidad');
+const categoria = document.getElementById('categoria');
+const totalPagar = document.getElementById('totalAPagar');
+const btnResumen = document.getElementById('resumen');
+
+//console.log(totalPagar);
+
+function calcularPago() {
+    let total = cantidad.value * ticket;
+
+//condicional por descuentos
+    switch (categoria.value) {
+        case "estudiante":
+            total = total - (total * descEstudiante);
+            break;
+        case "trainee":
+            total = total - (total * descTrainee);
+            break;
+        case "junior":
+            total = total - (total * descJunior);
+            break;
+    }
+    totalPagar.textContent = `Total a Pagar: $ ${total}`;
+}
+btnResumen.addEventListener('click', (e) => {
+    e.preventDefault();
+    calcularPago();
+});
